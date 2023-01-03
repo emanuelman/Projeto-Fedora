ae7eb1662dc8baacab61aabff413db12965e9ff8
Emanuel Antunes <72533265+emanuelman@users.noreply.github.com>
2023-01-03 19:01:35 -0300

    Update fedora.py

--- fedora.py
+++ fedora.py
@@ -13,9 +13,9 @@ def configurar_firefox():
     pyautogui.keyUp('ctrl')
     pyautogui.write("https://accounts.google.com/v3/signin/identifier?dsh=S118291408%3A1672781021235183&continue=https%3A%2F%2Fwww.google.com%2Fsearch%3Fclient%3Dfirefox-b-d%26q%3Dsign%2Bin&ec=GAZAAQ&hl=pt-BR&passive=true&flowName=GlifWebSignIn&flowEntry=ServiceLogin&ifkv=AeAAQh6vuYkuIw9bcp8J_ZxRNin2f04dmFl2mhaBzP8ITxcPiNZPW1d8kW76WoURqnxOrjyjwf9V")
     pyautogui.press("enter")
-    pyautogui.write("emanuelantunes1020@gmail.com")
+    pyautogui.write("email") #email
     pyautogui.press("enter")
-    pyautogui.write("chmod777")
+    pyautogui.write("senha") #senha
     pyautogui.press("enter")
     pyautogui.press("esc")
     #Navegador FireFox
@@ -24,9 +24,9 @@ def configurar_firefox():
     pyautogui.keyUp('ctrl')
     pyautogui.write("https://accounts.firefox.com/?context=fx_desktop_v3&entrypoint=fxa_app_menu&action=email&service=sync")
     pyautogui.press("enter")
-    pyautogui.write("emanuelantunes1020@gmail.com")
+    pyautogui.write("email") #email
     pyautogui.press("enter")
-    pyautogui.write("chmod777")
+    pyautogui.write("senha") #senha
     pyautogui.press("enter")
     pyautogui.keyDown('ctrl')
     pyautogui.press("t")
@@ -44,9 +44,9 @@ def configurar_google():
     pyautogui.keyUp('ctrl')
     pyautogui.write("https://accounts.google.com/v3/signin/identifier?dsh=S118291408%3A1672781021235183&continue=https%3A%2F%2Fwww.google.com%2Fsearch%3Fclient%3Dfirefox-b-d%26q%3Dsign%2Bin&ec=GAZAAQ&hl=pt-BR&passive=true&flowName=GlifWebSignIn&flowEntry=ServiceLogin&ifkv=AeAAQh6vuYkuIw9bcp8J_ZxRNin2f04dmFl2mhaBzP8ITxcPiNZPW1d8kW76WoURqnxOrjyjwf9V")
     pyautogui.press("enter")
-    pyautogui.write("emanuelantunes1020@gmail.com")
+    pyautogui.write("email") #email
     pyautogui.press("enter")
-    pyautogui.write("chmod777")
+    pyautogui.write("senha") #senha
     pyautogui.press("enter")
     pyautogui.press("esc")
 
