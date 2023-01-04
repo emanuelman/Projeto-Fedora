c92cb8a81f6e570fa16b30f1153f08f5952c41e6
Emanuel Antunes <72533265+emanuelman@users.noreply.github.com>
2023-01-04 18:13:48 -0300

    Update fedora.sh

--- fedora.sh
+++ fedora.sh
@@ -152,6 +152,7 @@ instalar zsh && ohmyzsh
 "
 sleep 1s
 cd ~
+#Assim que te pedir a confirmacao de que é para usar o 'zsh' como padrao digite 'yes' e logo em seguida digite 'exit' para o scrip continuar executando
 sudo dnf install util-linux-user -y
 sudo dnf install zsh -y
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" -y
