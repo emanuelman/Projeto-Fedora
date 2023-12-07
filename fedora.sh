69c1637650fcf34e0a151b4eafcb2867dcad6c9e
Emanuel Antunes <72533265+emanuelman@users.noreply.github.com>
2023-12-06 22:39:49 -0300

    Update fedora.sh

--- fedora.sh
+++ fedora.sh
@@ -190,6 +190,7 @@ cd ~
     sudo dnf install vim -y
     git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
     sh ~/.vim_runtime/install_awesome_vimrc.sh
+    # :w !sudo tee %
 }
 
 function verificar(){
